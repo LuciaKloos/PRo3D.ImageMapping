7090f197cdc335d9e94e385a90ccc87919f7bb18
Lucia Kloos <e12122563@student.tuwien.ac.at>
2026-06-09 13:11:43 +0200

    3d scene with two different images mapped

--- src/PRo3D.InstrumentProjection/ImageProjection.fs
+++ src/PRo3D.InstrumentProjection/ImageProjection.fs
@@ -69,79 +69,43 @@ module ImageProjection =
 
         let stableImageProjection (v : Vertex) = 
             fragment {
-                let p = v.projectedPos.XYZ / v.projectedPos.W
-                let tc = V3d(0.5, 0.5,0.5) + V3d(0.5, 0.5, 0.5) * p.XYZ
-                let inRange = Vec.allGreaterOrEqual tc V3d.OOO && Vec.allSmallerOrEqual tc.XYZ V3d.III  // clip against projection volume 
+                let p = v.projectedPos.XYZ / v.projectedPos.W   // convert to ndc [-1,1]
+                let tc = V3d(0.5, 0.5,0.5) + V3d(0.5, 0.5, 0.5) * p.XYZ     // convert to texture coordinates [0,1]
+                let inRange = Vec.allGreaterOrEqual tc V3d.OOO && Vec.allSmallerOrEqual tc.XYZ V3d.III      // tests whether frag is in projector frustum
                 let borderWidth = 0.01 
 
+                // transforms the fragment’s normal into the projected camera’s coordinate system
                 let normal = uniform.ProjectedImageModelViewProj.TransformDir(v.localNormalNumericallyUnstable) |> Vec.normalize
-                
-                let primaryColor = V4d(v.c.XYZ)
-                let finalColor = primaryColor
+
                 let c = 
                     if uniform.ProjectedImageModelViewProjValid && inRange && normal.Z < 0.0 then
                         let AFC = V2d(1.0 - tc.Y, 1.0 - tc.X)
                         let HSH = V2d(1.0 - tc.Y, 1.0 - tc.X)
-                        let AFC2 = V2d(tc.X, tc.Y)
-                        let c = projectedTexture.Sample(AFC2).X |> Shaders.remap
+                        let AFC2 = V2d(tc.X, tc.Y)  // directly maps projected X and Y to texture U and V: no axis flipping
+                        let c = projectedTexture.Sample(AFC2).X |> Shaders.remap    // only takes red channel of the texture, which is sufficient for grayscale images and allows to use single channel textures for better memory efficiency
                         let xBorder = (smoothstep 0.0 borderWidth tc.X) * smoothstep 1.0 (1.0 - borderWidth) tc.X 
                         let yBorder = (smoothstep 0.0 borderWidth tc.Y) * smoothstep 1.0 (1.0 - borderWidth) tc.Y
                         let borderFactor = xBorder * yBorder
                         let borderColor = V3d(0.0, 1.0, 0.0)
                         let blendedProjected = Fun.Lerp(uniform.ProjectedImageOpacity, v.c.XYZ, c.XYZ)
                         let borderImage = blendedProjected.XYZ * borderFactor + borderColor * (1.0 - borderFactor)
-                        primaryColor = V4d(borderImage.XYZ, 1.0) 
-                    else if uniform.ProjectedImageModelViewProjValid &&
-                       inRange &&
-                       normal.Z > 0.65 then  // selects approximately one hemisphere relative to the projection camera --> becomes red
+                        V4d(borderImage.XYZ, 1.0) 
 
-                        let overlayP =
-                            v.overlayProjectedPos.XYZ /
-                            v.overlayProjectedPos.W
-
-                        let overlayTc =
-                            V3d(0.5, 0.5, 0.5) +
-                            V3d(0.5, 0.5, 0.5) * overlayP
-
-                        let overlayInRange =
-                            v.overlayProjectedPos.W > 0.0 &&
-                            Vec.allGreaterOrEqual overlayTc V3d.OOO &&
-                            Vec.allSmallerOrEqual overlayTc V3d.III
-
-                        let overlayNormal =
-                            uniform.OverlayImageModelViewProj
-                                .TransformDir(v.localNormalNumericallyUnstable)
-                            |> Vec.normalize
-
-                        finalColor =
-                            if uniform.OverlayImageModelViewProjValid &&
-                                overlayInRange &&
-                                overlayNormal.Z > 0.65 then
-
-                                let overlaySample =
-                                    overlayProjectedTexture
-                                        .Sample(V2d(overlayTc.X, overlayTc.Y))
-                                        .X
-                                    |> Shaders.remap
-
-                                let blendedOverlay =
-                                    Fun.Lerp(
-                                        uniform.ProjectedImageOpacity,
-                                        primaryColor.XYZ,
-                                        overlaySample.XYZ
-                                    )
-
-                                V4d(blendedOverlay, 1.0)
-                            else
-                                primaryColor
-                    else 
-                        finalColor = v.c
-                        
-                   
-                return {
-                    v with
-                        c = finalColor
-                }
+                    else if uniform.ProjectedImageModelViewProjValid && inRange && normal.Z > 0.0 then
+                        let AFC = V2d(1.0 - tc.Y, 1.0 - tc.X)
+                        let HSH = V2d(1.0 - tc.Y, 1.0 - tc.X)
+                        let AFC2 = V2d(tc.X, tc.Y)  // directly maps projected X and Y to texture U and V: no axis flipping
+                        let c = overlayProjectedTexture.Sample(AFC2).X |> Shaders.remap    // only takes red channel of the texture, which is sufficient for grayscale images and allows to use single channel textures for better memory efficiency
+                        let xBorder = (smoothstep 0.0 borderWidth tc.X) * smoothstep 1.0 (1.0 - borderWidth) tc.X 
+                        let yBorder = (smoothstep 0.0 borderWidth tc.Y) * smoothstep 1.0 (1.0 - borderWidth) tc.Y
+                        let borderFactor = xBorder * yBorder
+                        let borderColor = V3d(0.0, 1.0, 0.0)
+                        let blendedProjected = Fun.Lerp(uniform.ProjectedImageOpacity, v.c.XYZ, c.XYZ)
+                        let borderImage = blendedProjected.XYZ * borderFactor + borderColor * (1.0 - borderFactor)
+                        V4d(borderImage.XYZ, 1.0) 
+                    else
+                        v.c
+                return { v with c = c }
             }
 
         [<ReflectedDefinition>]
